120bb510c30af587d395d9ce52460d448d7daee6
schang1146 <sammychang2185@gmail.com>
2019-06-21 20:27:06 -0700

    added font-awesome as an option; updated styling

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -16,8 +16,8 @@ class Todo extends Component {
 
     render() {
         return (
-            <ListGroupItem className={`todo-item${this.props.todo.completed ? ' completed' : ''}`} onClick={this.handleCompleteTodo}>
-                <p>{this.props.todo.value}</p>
+            <ListGroupItem onClick={this.handleCompleteTodo}>
+                <div className={`todo-item${this.props.todo.completed ? ' completed' : ''}`}>{this.props.todo.value}</div>
                 <Button color='danger' onClick={this.handleDeleteTodo}>delete</Button>
             </ListGroupItem>
         )
