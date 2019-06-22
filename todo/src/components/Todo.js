2cc48a22726a31f4a1fb08895ec0b514c8886437
schang1146 <sammychang2185@gmail.com>
2019-06-21 20:42:57 -0700

    added checkbox if todo is completed

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -17,7 +17,7 @@ class Todo extends Component {
     render() {
         return (
             <ListGroupItem onClick={this.handleCompleteTodo}>
-                <div className={`todo-item${this.props.todo.completed ? ' completed' : ''}`}>{this.props.todo.value}</div>
+                <div className={`todo-item${this.props.todo.completed ? ' completed' : ''}`}>{this.props.todo.value} <i className={`far fa-check-square${this.props.todo.completed ? ' completed' : ' hidden'}`}></i></div>
                 <Button color='danger' onClick={this.handleDeleteTodo}>delete</Button>
             </ListGroupItem>
         )
